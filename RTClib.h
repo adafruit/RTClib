c88573dab26cb831f168474798c147da03d5be24
Tony DiCola <tony@tonydicola.com>
2014-06-12 12:03:46 -0700

    Add TimeSpan class and simple DateTime duration arithmetic support.

--- RTClib.h
+++ RTClib.h
@@ -4,12 +4,15 @@
 #ifndef _RTCLIB_H_
 #define _RTCLIB_H_
 
+class TimeSpan;
+
 // Simple general-purpose date/time class (no TZ / DST / leap second handling!)
 class DateTime {
 public:
     DateTime (uint32_t t =0);
     DateTime (uint16_t year, uint8_t month, uint8_t day,
                 uint8_t hour =0, uint8_t min =0, uint8_t sec =0);
+    DateTime (const DateTime& copy);
     DateTime (const char* date, const char* time);
     DateTime (const __FlashStringHelper* date, const __FlashStringHelper* time);
     uint16_t year() const       { return 2000 + yOff; }
@@ -25,8 +28,31 @@ public:
     // 32-bit times as seconds since 1/1/1970
     uint32_t unixtime(void) const;
 
+    DateTime operator+(const TimeSpan& span);
+    DateTime operator-(const TimeSpan& span);
+    TimeSpan operator-(const DateTime& right);
+
 protected:
     uint8_t yOff, m, d, hh, mm, ss;
+};
+
+// Timespan which can represent changes in time with seconds accuracy.
+class TimeSpan {
+public:
+    TimeSpan (int32_t seconds = 0);
+    TimeSpan (int16_t days, int8_t hours, int8_t minutes, int8_t seconds);
+    TimeSpan (const TimeSpan& copy);
+    int16_t days() const         { return _seconds / 86400L; }
+    int8_t  hours() const        { return _seconds / 3600 % 24; }
+    int8_t  minutes() const      { return _seconds / 60 % 60; }
+    int8_t  seconds() const      { return _seconds % 60; }
+    int32_t totalseconds() const { return _seconds; }
+
+    TimeSpan operator+(const TimeSpan& right);
+    TimeSpan operator-(const TimeSpan& right);
+
+protected:
+    int32_t _seconds;
 };
 
 // RTC based on the DS1307 chip connected via I2C and the Wire library
