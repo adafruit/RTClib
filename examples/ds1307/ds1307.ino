e03a97139e285eeb4a5a3c052ab421f53a88031c
ladyada <limor@ladyada.net>
2016-09-05 21:09:43 -0400

    dont autoadjust

--- examples/ds1307/ds1307.ino
+++ examples/ds1307/ds1307.ino
@@ -18,7 +18,7 @@ void setup () {
   if (! rtc.isrunning()) {
     Serial.println("RTC is NOT running!");
     // following line sets the RTC to the date & time this sketch was compiled
-    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+    // rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
     // This line sets the RTC with an explicit date & time, for example to set
     // January 21, 2014 at 3am you would call:
     // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
