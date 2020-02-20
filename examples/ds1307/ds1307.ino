04e31eb7ffd0b8d5dc70e855ddc2150698a6e689
Daniel Lilja <60037035+DanielFrykman@users.noreply.github.com>
2020-02-20 08:37:59 +0100

    Comments indented one level less.
    The section is indented one level less. So it follows the void setup ()

--- examples/ds1307/ds1307.ino
+++ examples/ds1307/ds1307.ino
@@ -17,11 +17,11 @@ void setup () {
   if (! rtc.isrunning()) {
     Serial.println("RTC is NOT running!");
   }
-    // following line sets the RTC to the date & time this sketch was compiled
-    // rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-    // This line sets the RTC with an explicit date & time, for example to set
-    // January 21, 2014 at 3am you would call:
-    // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
+  // following line sets the RTC to the date & time this sketch was compiled
+  // rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  // This line sets the RTC with an explicit date & time, for example to set
+  // January 21, 2014 at 3am you would call:
+  // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
 }
 
 void loop () {
