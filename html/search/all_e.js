b00832dde0c590568e6a8c0b16003870325e802e
Doxygen CI <travis@travis-ci.org>
2020-04-16 13:39:24 +0000

    Deploy code docs to GitHub Pages Travis build: 59
    Commit: 23f904a0869243176bfe0eba912d0353c2cbd1cd

--- html/search/all_e.js
+++ html/search/all_e.js
@@ -4,7 +4,7 @@ var searchData=
   ['seconds',['seconds',['../class_time_span.html#ad1860591ff7f8c021c6bbadcc6d9ec02',1,'TimeSpan']]],
   ['seconds_5ffrom_5f1970_5fto_5f2000',['SECONDS_FROM_1970_TO_2000',['../_r_t_clib_8h.html#a1a4c3c6b78fa47471646f18b572ddd46',1,'RTClib.h']]],
   ['seconds_5fper_5fday',['SECONDS_PER_DAY',['../_r_t_clib_8h.html#a427ffe4394306d962b8972b3bc952e1c',1,'RTClib.h']]],
-  ['secondstime',['secondstime',['../class_date_time.html#adae8d9c0249f99944bcec7701bd2937e',1,'DateTime']]],
+  ['secondstime',['secondstime',['../class_date_time.html#a15a271a5c566550e3feae2b9624430f0',1,'DateTime']]],
   ['setalarm1',['setAlarm1',['../class_r_t_c___d_s3231.html#ad12211e56637221b0c6b956bfe764404',1,'RTC_DS3231']]],
   ['setalarm2',['setAlarm2',['../class_r_t_c___d_s3231.html#a686299444985b8c36d12a7ce32c9add6',1,'RTC_DS3231']]],
   ['ss',['ss',['../class_date_time.html#a38da3481e6355fcf8ceb4c57eb9db0ce',1,'DateTime']]]
