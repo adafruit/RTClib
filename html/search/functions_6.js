4e3513228b7da956384a653990ed3b88bdd56e51
Doxygen CI <travis@travis-ci.org>
2020-04-20 18:23:06 +0000

    Deploy code docs to GitHub Pages Travis build: 65
    Commit: af835898fd0dc177fb13694e87498ef8451eb85b

--- html/search/functions_6.js
+++ html/search/functions_6.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['initialized',['initialized',['../class_r_t_c___p_c_f8523.html#aa2f261383ef95f88108405f5d7ae4e2f',1,'RTC_PCF8523']]],
   ['ispm',['isPM',['../class_date_time.html#a0c11137c3e56e1b7cc7a6dccee755b8e',1,'DateTime']]],
-  ['isrunning',['isrunning',['../class_r_t_c___d_s1307.html#a0ec6f600f705edfef43bd798dce9946f',1,'RTC_DS1307']]]
+  ['isrunning',['isrunning',['../class_r_t_c___d_s1307.html#a0ec6f600f705edfef43bd798dce9946f',1,'RTC_DS1307']]],
+  ['isvalid',['isValid',['../class_date_time.html#a21b2a59fbde555e5c5688d44e9d95aec',1,'DateTime']]]
 ];
