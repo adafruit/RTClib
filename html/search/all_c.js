fd7f1f25f132c0a44c09ec20c71df505ebdb2f49
Doxygen CI <travis@travis-ci.org>
2020-04-28 21:10:44 +0000

    Deploy code docs to GitHub Pages Travis build:
    Commit:

--- html/search/all_c.js
+++ html/search/all_c.js
@@ -4,6 +4,7 @@ var searchData=
   ['pcf8523_5fclkoutcontrol',['PCF8523_CLKOUTCONTROL',['../_r_t_clib_8h.html#a67871d89f1920336a3236bc9242d9adc',1,'RTClib.h']]],
   ['pcf8523_5fcontrol_5f3',['PCF8523_CONTROL_3',['../_r_t_clib_8h.html#ac7ccab90cfcb90f39c7ffee193e8aa5f',1,'RTClib.h']]],
   ['pcf8523_5foffset',['PCF8523_OFFSET',['../_r_t_clib_8h.html#a5d912786bf7684920de9c17d2332b9be',1,'RTClib.h']]],
+  ['pcf8523_5fstatusreg',['PCF8523_STATUSREG',['../_r_t_clib_8h.html#afc73dd7806192e16dcae13315f88bfb4',1,'RTClib.h']]],
   ['pcf8523offsetmode',['Pcf8523OffsetMode',['../_r_t_clib_8h.html#ab02090fa3c1937fe68c0f036beae351f',1,'RTClib.h']]],
   ['pcf8523sqwpinmode',['Pcf8523SqwPinMode',['../_r_t_clib_8h.html#ab06ba706f2d9f1e59b1ce1fe3048c54c',1,'RTClib.h']]],
   ['progmem',['PROGMEM',['../_r_t_clib_8cpp.html#a07f7414a12e65e2a192193542b7f13da',1,'RTClib.cpp']]]
--- html/search/defines_2.js
+++ html/search/defines_2.js
@@ -3,5 +3,6 @@ var searchData=
   ['pcf8523_5faddress',['PCF8523_ADDRESS',['../_r_t_clib_8h.html#ad25274a8c8a482383c086226b505ec27',1,'RTClib.h']]],
   ['pcf8523_5fclkoutcontrol',['PCF8523_CLKOUTCONTROL',['../_r_t_clib_8h.html#a67871d89f1920336a3236bc9242d9adc',1,'RTClib.h']]],
   ['pcf8523_5fcontrol_5f3',['PCF8523_CONTROL_3',['../_r_t_clib_8h.html#ac7ccab90cfcb90f39c7ffee193e8aa5f',1,'RTClib.h']]],
-  ['pcf8523_5foffset',['PCF8523_OFFSET',['../_r_t_clib_8h.html#a5d912786bf7684920de9c17d2332b9be',1,'RTClib.h']]]
+  ['pcf8523_5foffset',['PCF8523_OFFSET',['../_r_t_clib_8h.html#a5d912786bf7684920de9c17d2332b9be',1,'RTClib.h']]],
+  ['pcf8523_5fstatusreg',['PCF8523_STATUSREG',['../_r_t_clib_8h.html#afc73dd7806192e16dcae13315f88bfb4',1,'RTClib.h']]]
 ];
