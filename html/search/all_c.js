61e698af3ed8f50b41000ed98ae126204dd000df
Doxygen CI <ci-arduino@invalid>
2021-12-07 14:54:48 +0000

    Deploy docs to GitHub Pages from commit b4791ebcaa
    Commit: b4791ebcaacee207e48987ac77ae562d6d85af59
    GitHub Actions run: 1549916994

--- html/search/all_c.js
+++ html/search/all_c.js
@@ -1,8 +1,8 @@
 var searchData=
 [
   ['operator_21_3d',['operator!=',['../class_date_time.html#aa64131ebd35cb40ed616b111ea501760',1,'DateTime']]],
-  ['operator_2b',['operator+',['../class_date_time.html#ae043350a8bfa362aabbc9f5ee658b4e6',1,'DateTime::operator+()'],['../class_time_span.html#af1dad98bdfcf07cf668e18509fbca8d4',1,'TimeSpan::operator+()']]],
-  ['operator_2d',['operator-',['../class_date_time.html#acbdcc7a6b55924552e72696d9e5bbb98',1,'DateTime::operator-(const TimeSpan &amp;span)'],['../class_date_time.html#a005fb6b5723a393da66e821a93cec5a5',1,'DateTime::operator-(const DateTime &amp;right)'],['../class_time_span.html#afe69b3603f24bcbeb16a20e5f97af79b',1,'TimeSpan::operator-()']]],
+  ['operator_2b',['operator+',['../class_date_time.html#a8d6b85fa26106369e81da7b824e78936',1,'DateTime::operator+()'],['../class_time_span.html#a5121884a13304ebf2b4346e90e205938',1,'TimeSpan::operator+()']]],
+  ['operator_2d',['operator-',['../class_date_time.html#aef3bee1f4e9de8d79426f35db1e0dbe4',1,'DateTime::operator-(const TimeSpan &amp;span) const'],['../class_date_time.html#ac901785851a822ae9051c7916559198f',1,'DateTime::operator-(const DateTime &amp;right) const'],['../class_time_span.html#ae276db5e4710b105f5aa5b8c3710d5f1',1,'TimeSpan::operator-()']]],
   ['operator_3c',['operator&lt;',['../class_date_time.html#a6f1288d0205e97f86dfecfc7fd7b4e60',1,'DateTime']]],
   ['operator_3c_3d',['operator&lt;=',['../class_date_time.html#af9da2ce38916caa365f24a38f5834b47',1,'DateTime']]],
   ['operator_3d_3d',['operator==',['../class_date_time.html#a0c68b136471b6f8daf575ce399ca619c',1,'DateTime']]],
--- html/search/functions_10.js
+++ html/search/functions_10.js
@@ -1,6 +1,6 @@
 var searchData=
 [
   ['write_5fregister',['write_register',['../class_r_t_c___i2_c.html#a6ae48628766373ce608c61a71fcbdd3d',1,'RTC_I2C']]],
-  ['writenvram',['writenvram',['../class_r_t_c___d_s1307.html#ac60cf11ebb0a4dc1fe6097787caecd75',1,'RTC_DS1307::writenvram(uint8_t address, uint8_t data)'],['../class_r_t_c___d_s1307.html#a9687ba44a4f9c75cc067806e38b237e9',1,'RTC_DS1307::writenvram(uint8_t address, uint8_t *buf, uint8_t size)']]],
+  ['writenvram',['writenvram',['../class_r_t_c___d_s1307.html#ac60cf11ebb0a4dc1fe6097787caecd75',1,'RTC_DS1307::writenvram(uint8_t address, uint8_t data)'],['../class_r_t_c___d_s1307.html#a3099cef99081a64bec954399a424ef61',1,'RTC_DS1307::writenvram(uint8_t address, const uint8_t *buf, uint8_t size)']]],
   ['writesqwpinmode',['writeSqwPinMode',['../class_r_t_c___d_s1307.html#a0d0871447dc3464dc8dc127433a14691',1,'RTC_DS1307::writeSqwPinMode()'],['../class_r_t_c___d_s3231.html#a7dd635277072b79d51d830a72dae1f72',1,'RTC_DS3231::writeSqwPinMode()'],['../class_r_t_c___p_c_f8523.html#afac1866e69541cfb93251dee683f0e63',1,'RTC_PCF8523::writeSqwPinMode()'],['../class_r_t_c___p_c_f8563.html#aa69b313f7836d709a52ca013a4545c97',1,'RTC_PCF8563::writeSqwPinMode()']]]
 ];
