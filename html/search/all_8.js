4e25fc4e88dea55d5a5b233991a61cfd34615053
Doxygen CI <ci-arduino@invalid>
2022-05-14 15:58:07 +0000

    Deploy docs to GitHub Pages from commit 49af24426b
    Commit: 49af24426bbe17dcc8d9a597bb4ce3f5771640a8
    GitHub Actions run: 2324427600

--- html/search/all_8.js
+++ html/search/all_8.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['i2c_5fdev',['i2c_dev',['../class_r_t_c___i2_c.html#a9b4e0ae88fdf27b1bf52cf49b71a1676',1,'RTC_I2C']]],
-  ['initialized',['initialized',['../class_r_t_c___p_c_f8523.html#aa2f261383ef95f88108405f5d7ae4e2f',1,'RTC_PCF8523']]],
+  ['initialized',['initialized',['../class_r_t_c___p_c_f8523.html#a739c2c1d770f661da06ee7afe5886d62',1,'RTC_PCF8523']]],
   ['isenabled32k',['isEnabled32K',['../class_r_t_c___d_s3231.html#ada935a4541328b627b4dbba95fa133c8',1,'RTC_DS3231']]],
   ['ispm',['isPM',['../class_date_time.html#a0c11137c3e56e1b7cc7a6dccee755b8e',1,'DateTime']]],
   ['isrunning',['isrunning',['../class_r_t_c___d_s1307.html#a0ec6f600f705edfef43bd798dce9946f',1,'RTC_DS1307::isrunning()'],['../class_r_t_c___p_c_f8523.html#a0dfb7557baf6f32a49fc71f45b19095c',1,'RTC_PCF8523::isrunning()'],['../class_r_t_c___p_c_f8563.html#a0b12f578c46fdcca20df2340a7039c6f',1,'RTC_PCF8563::isrunning()']]],
--- html/search/functions_8.js
+++ html/search/functions_8.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['lostpower',['lostPower',['../class_r_t_c___d_s3231.html#a097dc15a79ec077073969432b1abcb88',1,'RTC_DS3231::lostPower()'],['../class_r_t_c___p_c_f8523.html#a47962a5ca20578497496df2ee979ca43',1,'RTC_PCF8523::lostPower()'],['../class_r_t_c___p_c_f8563.html#ab2de6ea3cc5712efc02cb62589d912ce',1,'RTC_PCF8563::lostPower()']]]
+  ['lostpower',['lostPower',['../class_r_t_c___d_s3231.html#a097dc15a79ec077073969432b1abcb88',1,'RTC_DS3231::lostPower()'],['../class_r_t_c___p_c_f8523.html#a28845cecdab25b0e5620c37a610cae84',1,'RTC_PCF8523::lostPower()'],['../class_r_t_c___p_c_f8563.html#ae264d5b935bd313b8ad4be8c319080c3',1,'RTC_PCF8563::lostPower()']]]
 ];
