e57ec315300d1aad4abbb5e38a434cf9ecba5b0c
Doxygen CI <ci-arduino@invalid>
2022-08-09 15:06:02 +0000

    Deploy docs to GitHub Pages from commit a944a787d7
    Commit: a944a787d7dc853b6c0e3a8cd127c1b09a424a06
    GitHub Actions run: 2826182958

--- html/search/functions_5.js
+++ html/search/functions_5.js
@@ -1,4 +1,8 @@
 var searchData=
 [
+  ['getalarm1',['getAlarm1',['../class_r_t_c___d_s3231.html#aa8079cc5cedf5ad3178fd8f6b0f2c25c',1,'RTC_DS3231']]],
+  ['getalarm1mode',['getAlarm1Mode',['../class_r_t_c___d_s3231.html#af22bd04a5b78398f35c11207f9fc266b',1,'RTC_DS3231']]],
+  ['getalarm2',['getAlarm2',['../class_r_t_c___d_s3231.html#aa7075b5983e9f61175d1335d9796b272',1,'RTC_DS3231']]],
+  ['getalarm2mode',['getAlarm2Mode',['../class_r_t_c___d_s3231.html#a74aaeb9b1b27cc07b0828f20f7a56b4c',1,'RTC_DS3231']]],
   ['gettemperature',['getTemperature',['../class_r_t_c___d_s3231.html#a6eb180c4c14a22322a9ea3e58b51798b',1,'RTC_DS3231']]]
 ];
